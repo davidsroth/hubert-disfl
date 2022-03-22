93da8b30f8859702689b55048131556d673fab9f
David S Roth <davidroth6@gmail.com>
2022-03-22 12:46:47 -0400

    quickstart run success

--- model.py
+++ model.py
@@ -1,17 +1,18 @@
-from transformers import Wav2Vec2Processor, HubertModel, HubertConfig
+from transformers import Wav2Vec2Processor, HubertForCTC, HubertConfig
 from datasets import load_dataset
 import soundfile as sf
+import torch
 
 
 processor = Wav2Vec2Processor.from_pretrained('facebook/hubert-large-ls960-ft')
-model = HubertModel.from_pretrained("facebook/hubert-large-ls960-ft")
+model = HubertForCTC.from_pretrained("facebook/hubert-large-ls960-ft")
 
 dataset = load_dataset("hf-internal-testing/librispeech_asr_demo", "clean", split="validation")
 dataset=dataset.sort("id")
 sampling_rate = dataset.features["audio"].sampling_rate
 
 # audio file is decoded on the fly
-inputs = processor(dataset[0]["audio"]["array"], sampling_rate=sampling_rate, return_tensors="pt")
+inputs = processor(dataset[1]["audio"]["array"], sampling_rate=sampling_rate, return_tensors="pt")
 with torch.no_grad():
     logits = model(**inputs).logits
 
