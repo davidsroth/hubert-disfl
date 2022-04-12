3a214d2c2f251b4b3ba8a92c6eb9c5b9f2d39705
davidsroth <davidroth6@gmail.com>
2022-04-12 13:06:22 -0400

    utils for loading audio for list of conversation_ids, start and end times

--- audio_utils.py
+++ audio_utils.py
@@ -1,19 +1,52 @@
 # -*- coding: utf-8 -*-
 # !pip install librosa
 import librosa
-import librosa.display # You need this in librosa to be able to plot
+# import librosa.display # You need this in librosa to be able to plot
 import os
 # import matplotlib.pyplot as plt
 
-clip_dir = os.path.join("sw02005.sph")
+# clip_dir = os.path.join("sw02005.sph")
 
-def encode_data(dataset, audio_tokenizer):
-  pass
+PROJECT_ROOT = os.environ.get("PROJECT_ROOT")
+SWB_ROOT = f"{PROJECT_ROOT}/switchboard"
+SWB_DATA_ROOT = f"{SWB_ROOT}/disks"
+DISK_MAP_PATH = f"{SWB_ROOT}/docs/swb1_all.dvd.tbl"
+  
+def get_conversation_filepath(conversation_id, lookup):
+  return os.path.join(SWB_DATA_ROOT, lookup[conversation_id], f"{conversation_id}.sph")
 
-def load_audio_slice(filepath, start, end):#function to splice audio start=start time , end = end time in seconds
-  audio,sr = librosa.load(clip_dir,sr=8000,offset=start,duration=end) # audio is a numpy array
+def get_conversation_slice(conversation_id, start, end, target_sr, lookup):#function to splice audio start=start time , end = end time in seconds
+  file_path = get_conversation_filepath(conversation_id, lookup)
+  audio,sr = librosa.load(file_path,sr=8000,offset=start,duration=end) # audio is a numpy array
+  if sr != target_sr:
+    audio = resample(audio, sr, target_sr)
+    sr = target_sr
   return audio,sr
 
+def build_disk_lookup_table(file_path):
+  with open(file_path, 'r') as f:
+    content = f.read()
+  
+  lookup = {}
+  for line in content:
+    disk, conversation_id = line.split()
+    conversation_id = conversation_id.split(".")[0]
+    lookup[conversation_id] = disk
+  return lookup
+
+def resample(audio, source_sr, target_sr):
+  if source_sr == target_sr:
+    return audio
+  return librosa.resample(audio, source_sr, target_sr)
+
+def extract_inputs(dataset, target_sr):
+  lookup = build_disk_lookup_table(DISK_MAP_PATH)
+  inputs = []
+  for conversation_id, start_time, end_time in dataset[['conversation_id', 'start_time', 'end_time']].itertuples():
+    inputs.append(get_conversation_slice(conversation_id, start_time, end_time, target_sr, lookup))
+  
+  return inputs
+  
 #example
 # audio,sr=load(1,4) # loads audio from t=1 to t=4 seconds
 # fig, ax = plt.subplots(figsize=(15,8))
