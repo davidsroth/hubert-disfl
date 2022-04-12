550f8f0c9640b7e0d273fb09af6b4c34d31e02c3
davidsroth <davidroth6@gmail.com>
2022-04-12 16:32:53 -0400

    get_conversation_ids function from file containing one conversation_id per row

--- text_utils.py
+++ text_utils.py
@@ -3,16 +3,23 @@ import sys
 import pandas as pd
 from parser import extract_all
 
-def extract_targets(conversation_ids, fluent=False):
+def extract(conversation_ids, fluent=False):
     targets = []
     for conversation_id in conversation_ids:
         targets.extend(extract_all.extract(conversation_id, return_fluent=fluent))
     
     return pd.DataFrame(targets)
 
+def get_conversation_ids_from_file(filepath):
+    """
+    fetches conversation_ids from text file containing one id per line.
+    """
+    with open(filepath, 'r') as f:
+        conversation_ids = f.read().splitlines()
+    return conversation_ids
+
 if __name__ == "__main__":
     conversation_ids_file = sys.argv[1]
 
-    with open(conversation_ids_file, 'r') as f:
-        conversation_ids = f.read().splitlines()
-    print(extract_targets(conversation_ids))
+    conversation_ids = get_conversation_ids_from_file(conversation_ids_file)
+    print(extract(conversation_ids))
