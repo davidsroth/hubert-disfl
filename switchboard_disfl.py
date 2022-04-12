7d59331352b4ec1ed93ec4e6cdfe1afd43558308
davidsroth <davidroth6@gmail.com>
2022-04-12 16:33:24 -0400

    perform resampling at batch-time

--- switchboard_disfl.py
+++ switchboard_disfl.py
@@ -6,17 +6,22 @@ class SwitchboardDisfluencyDataset(Dataset):
     """
     A torch.utils.data.Dataset wrapper for the Switchboard dataset with Disfluency annotations.
     """
-    def __init__(self, conversation_ids):
-        self.targets = text_utils.extract_targets(conversation_ids)
-        self.input = audio_utils.extract_inputs(self.targets)
+    def __init__(self, conversation_ids, target_sr=16000):
+        self.target_sr = 16000
+        self.data = text_utils.extract(conversation_ids)
         assert len(self.targets) == len(self.input)
         
-
     def __len__(self):
         return len(self.targets)
 
     def __getitem__(self, i):
+        target = self.data[i]
         return {
-            'input_ids': self.input_ids[i],
-            'target': self.targets[i]
+            'audio': audio_utils.get_conversation_slice(
+                target["conversation_id"],
+                target['start_time'],
+                target['end_time'],
+                self.target_sr
+            ),
+            'target_text': self.data[i]["target_text"],
         }
