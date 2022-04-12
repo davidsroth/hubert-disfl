cbcc4de04c8b75e0e61dbe76800ed23bbb889ef1
davidsroth <davidroth6@gmail.com>
2022-04-12 12:55:56 -0400

    extract targets for list of conversation_ids

--- text_utils.py
+++ text_utils.py
@@ -1,5 +1,18 @@
+# import torch
+import sys
 import pandas as pd
-import torch
+from parser import extract_all
 
-def extract_targets(dataset):
-    pass
+def extract_targets(conversation_ids, fluent=False):
+    targets = []
+    for conversation_id in conversation_ids:
+        targets.extend(extract_all.extract(conversation_id, return_fluent=fluent))
+    
+    return pd.DataFrame(targets)
+
+if __name__ == "__main__":
+    conversation_ids_file = sys.argv[1]
+
+    with open(conversation_ids_file, 'r') as f:
+        conversation_ids = f.read().splitlines()
+    print(extract_targets(conversation_ids))
