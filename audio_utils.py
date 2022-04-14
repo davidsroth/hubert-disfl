b12fc9ab5cfc9a3618bb47f81e7b841fd1b1aa51
davidsroth <davidroth6@gmail.com>
2022-04-13 21:06:09 -0400

    preprocess on first read of each file

--- audio_utils.py
+++ audio_utils.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 # !pip install librosa
+from doctest import OutputChecker
 from responses import target
 import librosa
+import soundfile as sf
 # import librosa.display # You need this in librosa to be able to plot
 import os
 # import matplotlib.pyplot as plt
@@ -11,6 +13,7 @@ import os
 PROJECT_ROOT = os.environ.get("PROJECT_ROOT")
 SWB_ROOT = f"{PROJECT_ROOT}/switchboard"
 SWB_DATA_ROOT = f"{SWB_ROOT}/disks"
+UPSAMPLED_DATA_ROOT = f"{SWB_ROOT}/upsampled/disks"
 DISK_MAP_PATH = f"{SWB_ROOT}/docs/swb1_all.dvd.tbl"
   
 def build_disk_lookup_table(file_path):
@@ -29,7 +32,12 @@ lookup_table = build_disk_lookup_table(DISK_MAP_PATH)
 
 def get_conversation_filepath(conversation_id):
   conv_num = conversation_id[-4:]
-  return os.path.join(SWB_DATA_ROOT, lookup_table[conv_num], "data", f"sw0{conv_num}.sph")
+  if not os.path.exists(os.path.join(UPSAMPLED_DATA_ROOT, lookup_table[conv_num], "data", f"sw0{conv_num}.wav")):
+    result = preprocess(os.path.join(SWB_DATA_ROOT, lookup_table[conv_num], "data", f"sw0{conv_num}.sph"), os.path.join(UPSAMPLED_DATA_ROOT, lookup_table[conv_num], "data", f"sw0{conv_num}.wav"))
+    if result is not None:
+      raise Exception(f"Preprocessing failed for {conversation_id}")
+    
+  return os.path.join(UPSAMPLED_DATA_ROOT, lookup_table[conv_num], "data", f"sw0{conv_num}.sph")
 
 def get_conversation_slice(conversation_id, start, end, target_sr=16_000):#function to splice audio start=start time , end = end time in seconds
   file_path = get_conversation_filepath(conversation_id)
@@ -50,6 +58,17 @@ def extract_inputs(dataset, target_sr):
     inputs.append(sample)
   
   return inputs
+
+def preprocess(in_path, out_path, target_sr=16_000):
+  disk_path, file = os.path.split(out_path)
+  print(f"Preprocessing {file}",end='\r')
+  if not os.path.isdir(disk_path):
+    os.makedirs(disk_path)
+  audio, sr = librosa.load(in_path, sr=8_000)
+  resampled = resample(audio, sr, target_sr)
+  sf.write(out_path, resampled, target_sr)
+
+  return None
   
 #example
 # audio,sr=load(1,4) # loads audio from t=1 to t=4 seconds
